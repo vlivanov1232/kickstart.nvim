0d4d11d6daba2e77abb951084f67eb10b4c2b497
Vladimir Ivanov <vl.ivanov1232@gmail.com>
2024-01-20 02:23:15 +0400

    Added comments to keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -9,9 +9,15 @@ keymap.set('n', '<C-u>', '<C-u>zz', { noremap = true })
 keymap.set('n', '<C-o>', '<C-o>zz', { noremap = true })
 keymap.set('n', 'n', 'nzzzv', { noremap = true })
 keymap.set('n', 'N', 'Nzzzv', { noremap = true })
+
+--Open lazygit 
 keymap.set('n', '<leader>gg', ':LazyGit<CR>', { noremap = true, silent = true })
+
+--Telescope search with ripgrep args
 keymap.set('n', '<leader>sa', ':lua require("telescope").extensions.live_grep_args.live_grep_args()<CR>',
 	  { desc = '[S]earch with [A]rgs' })
+
+-- Open file explorer with Oil
 keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
 -- Harpoon
@@ -27,5 +33,6 @@ keymap.set("n", "<leader>h7", function() require("harpoon.ui").nav_file(7) end, 
 keymap.set("n", "<leader>h8", function() require("harpoon.ui").nav_file(8) end, { desc = "[H]arpoon 8" })
 keymap.set("n", "<leader>h9", function() require("harpoon.ui").nav_file(9) end, { desc = "[H]arpoon 9" })
 
+-- Move line with ALT + JK
 keymap.set("n", "<M-j>", ":m+1<CR>")
 keymap.set("n", "<M-k>", ":m-2<CR>")
